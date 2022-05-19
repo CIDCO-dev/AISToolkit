354140bd49c22dac1ef32a27957809d6b183bd5d
Jacobb13 <105226920+Jacobb13@users.noreply.github.com>
2022-05-19 14:31:31 -0400

    update install.sh
    modification

--- install.sh
+++ install.sh
@@ -18,8 +18,10 @@ echo "[+] Installing packages..."
 sudo apt install -y librtlsdr-dev libusb-dev libpthread-stubs0-dev ncat make pkg-config build-essential
 
 echo "[+] Cloning git repository"
+
+cd /home/ubuntu
 git clone https://github.com/dgiardini/rtl-ais
 
-cd home/ubuntu/rtl-ais
+cd /home/ubuntu/rtl-ais
 make
-cd ..
+cd..
