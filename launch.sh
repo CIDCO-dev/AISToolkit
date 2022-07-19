ed1df9cd0d027d176221aacd0da1d61485109524
Jacobb13 <jacob.bellavance@cidco.ca>
2022-07-19 09:20:20 -0400

    clean up

--- launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Installation Script for collecting AIS data on a raspberry pi device
-#Ubuntu version: 20.04.4 LTS
-#Launch execution script
-
-
-sudo /home/ubuntu/rtl-ais/rtl_ais -n -T
-
